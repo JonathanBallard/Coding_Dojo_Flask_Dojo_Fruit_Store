1620264a1018f46f2f290223a7857eb4111962a7
Jonathan Ballard <asgardschosen@gmail.com>
2020-01-20 21:24:29 -0800

    initial commit

--- server.py
+++ server.py
@@ -8,7 +8,16 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry = request.form['strawberry']
+    # blackberry = request.form['blackberry']
+    apple = request.form['apple']
+    raspberry = request.form['raspberry']
+    firstName = request.form['first_name']
+    lastName = request.form['last_name']
+    student_id = request.form['student_id']
+    totalOrder = int(raspberry) + int(apple) + int(strawberry)
+    print('charging ' + firstName + ' ' + lastName + ' for ' + str(totalOrder) + ' fruits')
+    return render_template("checkout.html", strawTemp = strawberry, raspTemp = raspberry, appleTemp = apple, totalTemp = totalOrder, firstTemp = firstName, lastTemp = lastName, studentTemp = student_id)
 
 @app.route('/fruits')         
 def fruits():
